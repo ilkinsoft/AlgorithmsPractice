8f37b6bff2ce5bdb482c562acfac45daf04cc975
İlkin Elimov <e-ilkin@msn.com>
2020-05-30 15:11:45 -0700

    Add project files.

---
+++ AlgorithmsPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
