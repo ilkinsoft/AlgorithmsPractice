6a6bec61a93841d576d86e96060ffe1d3b7a8e3d
İlkin Elimov <e-ilkin@msn.com>
2020-11-25 23:25:27 -0800

    QueueViaStacks problem solution is added.

---
+++ AlgorithmsPractice/Stacks-Queues/MyQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsPractice.Stacks_Queues
+{
+    public class MyQueue
+    {
+        private Stack<int> stack1 = new Stack<int>();
+        private Stack<int> stack2 = new Stack<int>();
+
+        public class MyNode
+        {
+            private int data;
+            public MyNode next;
+            public MyNode(int data)
+            {
+                this.data = data;
+            }
+        }
+        public void add(int data)
+        {
+            if (stack2.Count > 0)
+            {
+                foreach (var item in stack2)
+                    stack1.Push(stack2.Pop());
+            }
+            stack1.Push(data);
+        }
+        public int remove()
+        {
+            foreach (var item in stack1)
+                stack2.Push(stack1.Pop());
+
+            return stack2.Pop();
+        }
+    }
+}
